363329dc6e913113de3ff511c27bbd4c1a24c097
Tim Soul <stim14758@gmail.com>
2024-05-17 21:25:02 +0300

    Initial commit

---
+++ src/main/java/by/itclass/constants/AppConstants.java
@@ -0,0 +1,8 @@
+package by.itclass.constants;
+
+public class AppConstants {
+    public static final String ALL_USERS_URL = "/all";
+    public static final String USERS_LIST_ATTR = "usersList";
+    public static final String USERS_PAGE = "users";
+
+}
